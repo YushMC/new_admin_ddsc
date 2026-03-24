0319bf7808bbb709e5811032a1340588c6b2682c
YushMC <mujushaxb10@gmail.com>
2026-03-24 00:09:37 -0600

    second version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  app: {
+    baseURL: "/",
+  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/image", "@nuxt/ui"],
